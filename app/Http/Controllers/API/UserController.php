42f640fe284ce8fe020ae9c8b41c8f0ac7c2fbe7
Nurcholis Rio <colisrio@gmail.com>
2020-10-30 17:32:28 +0700

    change validate user controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
         try {
             $request->validate([
                 'name' => ['required', 'string', 'max:255'],
-                'email' => ['required', 'string', 'email','max:255', 'unique:users'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'password' => $this->passwordRules()
             ]);
 
