80f59366d66a0363f9be5236fea3d00ccbddb2dd
Nurcholis Rio <colisrio@gmail.com>
2020-10-31 15:48:49 +0700

    add views plant dashboard

--- resources/views/plant/index.blade.php
+++ resources/views/plant/index.blade.php
@@ -18,6 +18,7 @@
                         <tr>
                             <th class="border px-6 py-4">ID</th>
                             <th class="border px-6 py-4">Name</th>
+                            <th class="border px-6 py-4">Description</th>
                             <th class="border px-6 py-4">Price</th>
                             <th class="border px-6 py-4">Rate</th>
                             <th class="border px-6 py-4">Types</th>
@@ -29,6 +30,7 @@
                         <tr>
                             <td class="border px-6 py-4">{{ $item->id }}</td>
                             <td class="border px-6 py-4">{{ $item->name }}</td>
+                            <td class="border px-6 py-4">{{ $item->description }}</td>
                             <td class="border px-6 py-4">{{ number_format($item->price) }}</td>
                             <td class="border px-6 py-4">{{ $item->rate }}</td>
                             <td class="border px-6 py-4">{{ $item->types }}</td>
@@ -36,7 +38,7 @@
                                 <a href="{{route('plant.edit',$item->id)}}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">
                                     Edit
                                 </a>
-                                <form action="{{ route('plant.destroy',$item->id) }}" method="POST" class="inline-bloc">
+                                <form action="{{ route('plant.destroy',$item->id) }}" method="POST" class="inline-block">
                                     {!! method_field('delete') .csrf_field() !!}
                                     <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded">
                                         Delete
@@ -46,7 +48,7 @@
                         </tr>
                         @empty
                             <tr>
-                                <td colspan="5" class="border text-center p-5">
+                                <td colspan="7" class="border text-center p-5">
                                     Data Tidak Ditemukan
                                 </td>
                             </tr>
