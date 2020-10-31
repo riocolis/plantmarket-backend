6ba1ef4497f7a19e536893955c67d7b87d7c405f
Nurcholis Rio <colisrio@gmail.com>
2020-10-31 17:10:02 +0700

    add hash password in user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -42,6 +43,7 @@ class UserController extends Controller
     {
         $data = $request->all();
 
+        $data['password'] = Hash::make($request->password);
         $data['profile_photo_path'] = $request->file('profile_photo_path')->store('assets/user', 'public');
 
         User::create($data);
